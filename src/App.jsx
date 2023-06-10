0d9635b60181e43ac48008257af181cd8ba95802
Marveeen <jmvroqueee@gmail.com>
2023-06-10 09:54:36 +0800

    feature: add image and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import logo from "./assets/images/logo.png";
+
 function App() {
-  <div>
-    <h1>Hello</h1>
-  </div>;
+  return (
+    <>
+      <header>
+        <div id="logo-img">
+          <img src={logo} alt="LOGO" />
+        </div>
+        <h1>Learn Github Actions</h1>
+      </header>
+    </>
+  );
 }
 
 export default App;
